420722961d527a107929412f551e6366977b6b56
guevarafranklin <tecguevara@gmail.com>
2025-06-26 02:21:23 -0600

    created a plan and apply commands for s3 buckets.

---
+++ practice-3/bucket.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "my_bucket" {
+count = 5
+  bucket = "ringmehere-${random_string.sufijo[count.index].id}"
+
+  tags = {
+    "admin" = "admin"
+  }
+}
---
+++ practice-3/terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+provider "aws" {
+  region = "us-east-1"  # Change this to your preferred region
+}
+
+provider "random" {}
